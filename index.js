52ef5445405f4e45ab7d3d5468eb0a23a19650ab
tom <tom.garne@uwe.ac.uk>
2020-05-04 10:58:06 +0100

    fixed typos

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
-app.use (express.static(__dirname + '/publid'));
+app.use (express.static(__dirname + '/public'));
 
-fuction onConnection(socket){
-    socket.on('drawing', (data) => socket.braodcast.emit('drawing', data));
+function onConnection(socket){
+    socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
 }
 
 io.on('connection', onConnection);
